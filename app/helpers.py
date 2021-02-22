d1d7dc3679595235df72bae5aa65b1bce0d0da99
Guilherme Pace <pace.gui@gmail.com>
2021-02-22 12:09:25 -0600

    initial S3 configuration

---
+++ app/helpers.py
@@ -0,0 +1,10 @@
+# FILE REQUIRED FOR AWS S3
+
+import boto3, botocore
+from config import S3_KEY, S3_SECRET, S3_BUCKET
+
+s3 = boto3.client(
+    "s3",
+    aws_access_key_id=S3_KEY,
+    aws_secret_access_key=S3_SECRET
+)
