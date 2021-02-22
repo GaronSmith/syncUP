916bd2866c48ec5d54acb91c083d0c48871cf635
GaronSmith <wgaronsmith13@gmail.com>
2021-02-22 14:14:27 -0500

    added the remove user case to sessio

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -1,9 +1,17 @@
 const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser'
+
 
 export const setUser = (user) => {
     return {
         type: SET_USER,
         payload: user
+    }
+}
+
+export const removeUser = () => {
+    return {
+        type: REMOVE_USER
     }
 }
 
@@ -18,6 +26,10 @@ const sessionReducer = (state = initialState, action) => {
             newState = Object.assign({}, state);
             newState.user = action.payload
             return newState
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
         default:
             return state
     }
