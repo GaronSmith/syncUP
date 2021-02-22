073a7a3a2998d7ed0678c800321efa8782318c3c
GaronSmith <wgaronsmith13@gmail.com>
2021-02-22 13:05:09 -0500

    updating index to include store

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux"
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 
+import configureStore from "./store";
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = fetch;
+  window.store = store;
+  window.sessionActions = sessionActions;
+}
+
+function Root() {
+  return (
+    <Provider store = {store}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </Provider>
+  )
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
