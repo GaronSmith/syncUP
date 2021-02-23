481063bd875fd8160735751e184e39561f7f225b
Clint Hill <clint.hill07@gmail.com>
2021-02-23 14:04:48 -0500

    Make the profile page 'cardized'

--- react-app/src/components/UserProfile/ProfileBox.js
+++ react-app/src/components/UserProfile/ProfileBox.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 
-function ProfileBox({content}) {
+function userFileButton() {
+  return (
+  <>
+    <span> </span>
+    <input type='file'/>
+  </>
+  )
+}
+
+function ProfileBox({content, userFile}) {
   let [buttonText, setButtonText] = useState('Edit')
   let [value, setValue] = useState(content)
   let [formDisabled, setFormDisabled] = useState(true)
@@ -20,6 +29,7 @@ function ProfileBox({content}) {
       <input type='text' value={value} disabled={formDisabled} onChange={e => setValue(e.target.value)}/>
       <span> </span>
       <input type='button' value={buttonText} onClick={buttonClick}/>
+      {userFile && userFileButton()}
     </>
   )
 }
--- react-app/src/components/UserProfile/UserProfile.js
+++ react-app/src/components/UserProfile/UserProfile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProfileBox from './ProfileBox'
+import './UserProfile.css'
 
 const demoUser = {
   first_name: "Demo-Lition",
@@ -12,21 +13,34 @@ const demoUser = {
 function UserProfile() {
 
   return (
-    <div>
-      <img src={demoUser.img_url}/>
-      <h2>User Profile</h2>
-        <p>First Name <ProfileBox content={demoUser.first_name}/></p>
-        <p>Last Name <ProfileBox content={demoUser.last_name}/></p>
-        <p>Email Address <ProfileBox content={demoUser.email}/></p>
-        <p>Location <ProfileBox content={demoUser.location}/></p>
-        <p>Profile Picture <ProfileBox content={demoUser.img_url}/></p>
-        <p>Password <ProfileBox content={'*************'}/></p>
+    <div className='profile'>
+      <div className='profile_box flex'>
+        <div className='profile_user--left'>
+          <h2>User Profile</h2>
+          <p>First Name <ProfileBox content={demoUser.first_name}/></p>
+          <p>Last Name <ProfileBox content={demoUser.last_name}/></p>
+          <p>Email Address <ProfileBox content={demoUser.email}/></p>
+          <p>Location <ProfileBox content={demoUser.location}/></p>
+          <p>Profile Picture <ProfileBox content={demoUser.img_url} userFile={true}/></p>
+          <p>Password <ProfileBox content={'*************'}/></p>
+        </div>
+        <div className='profile_user--right'>
+          <div className='profile_picture' style={{ backgroundImage: `url(${demoUser.img_url})`}}/>
+        </div>
+      </div>
 
-      <h2>Groups</h2>
+      <div className='profile_box'>
+        <h2>Groups</h2>
+          <h3>--Events</h3>
+      </div>
 
-      <h2>Moderation Panel</h2>
+      <div className='profile_box'>
+        <h2>Moderation Panel</h2>
+      </div>
 
-      <h2>Administration Panel</h2>
+      <div className='profile_box'>
+        <h2>Administration Panel</h2>
+      </div>
 
     </div>
   )
