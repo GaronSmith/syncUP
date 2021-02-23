006eb633ba92409a62104d18d2d77cb006c99f0e
GaronSmith <wgaronsmith13@gmail.com>
2021-02-23 18:36:29 -0500

    created events reducer

--- react-app/src/components/LandingPage/SearchForm/SearchForm.js
+++ react-app/src/components/LandingPage/SearchForm/SearchForm.js
@@ -1,10 +1,18 @@
+import { useEffect } from 'react';
 import { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { searchEvents } from '../../../store/events';
 import './SearchForm.css'
 
 const  SearchForm = () => {
+    const dispatch = useDispatch
     const [searchValue, setSearchValue] = useState('');
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState()
+
+    useEffect(() => {
+        dispatch(searchEvents(searchValue))
+    },[searchValue, dispatch])
     return (
         <div className= 'search__container'>
             <h1 className= 'form__title'>Search for your next syncUP event</h1>
@@ -13,6 +21,9 @@ const  SearchForm = () => {
                     <input 
                     type='text' 
                     className='search__input'
+                    value= {searchValue}
+                    onChange= {(e) => setSearchValue(e.target.value)}
+                    />
                 </div>
             </form>
         </div>
---
+++ react-app/src/store/events.js
@@ -0,0 +1,36 @@
+const SET_EVENTS = 'events/setEvents'
+
+const setEvents = (events) => {
+    return {
+        type: SET_EVENTS,
+        payload: 
+    }
+}
+
+export const searchEvents(val) => async (dispatch) => {
+    const response = await fetch('/api/events', {
+        method:'POST',
+        body: JSON.stringify({val})
+    })
+    if(response.ok){
+        const events = await response.json()
+        dispatch(setEvents(events))
+    }
+}
+
+const initialState = { events: null };
+
+const eventsReducer = (state = initialState, action) => {
+
+    let newState;
+    switch (action.type) {
+        case SET_EVENTS:
+            newState = Object.assign({}, state);
+            newState.user = action.payload
+            return newState
+        default:
+            return state
+    }
+}
+
+export default eventsReducer
