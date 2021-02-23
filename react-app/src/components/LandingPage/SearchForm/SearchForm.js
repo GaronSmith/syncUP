d061f4d3fb735e574787a97c5928c41897424b37
GaronSmith <wgaronsmith13@gmail.com>
2021-02-23 17:29:47 -0500

    init

--- react-app/src/components/LandingPage/SearchForm/SearchForm.js
+++ react-app/src/components/LandingPage/SearchForm/SearchForm.js
@@ -1,10 +1,20 @@
+import { useState } from 'react'
 import './SearchForm.css'
 
 const  SearchForm = () => {
-
+    const [searchValue, setSearchValue] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState()
     return (
         <div className= 'search__container'>
             <h1 className= 'form__title'>Search for your next syncUP event</h1>
+            <form className='search_form'>
+                <div className='search__input-container'>
+                    <input 
+                    type='text' 
+                    className='search__input'
+                </div>
+            </form>
         </div>
     )
 }
