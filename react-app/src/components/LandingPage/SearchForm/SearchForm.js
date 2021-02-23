ecf68fdc54fb9384e428352f4ffb61234ddebdc6
GaronSmith <wgaronsmith13@gmail.com>
2021-02-23 16:39:05 -0500

    created searchform files

---
+++ react-app/src/components/LandingPage/SearchForm/SearchForm.js
@@ -0,0 +1,12 @@
+import './SearchForm.css'
+
+const  SearchForm = () => {
+
+    return (
+        <div className= 'search__container'>
+            <h1 className= 'form__title'>Search for your next syncUP event</h1>
+        </div>
+    )
+}
+
+export default SearchForm
