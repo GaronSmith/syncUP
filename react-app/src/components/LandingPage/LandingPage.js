d273a35e3036d0fda784468e81d9c982009fccd2
GaronSmith <wgaronsmith13@gmail.com>
2021-02-24 12:31:00 -0500

    added divs for landing page

--- react-app/src/components/LandingPage/LandingPage.js
+++ react-app/src/components/LandingPage/LandingPage.js
@@ -1,13 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 import './LandingPage.css'
 import SearchForm from './SearchForm'
 
 const LandingPage = () => {
+
+    const searchEvents = useSelector(state => state.events.search_results)
+    
     return (
         <div className='landing__container'>
             <SearchForm />
+            <div className='results_container'>
+                <div className='results__events'>
+
+                </div>
+                <div className='results__tags'>
+                    
+                </div>
+            </div>
         </div>
+
     )
 }
 
