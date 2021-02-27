9da480dfa443fd269552701ef508bfbb66bc396b
Clint Hill <clint.hill07@gmail.com>
2021-02-27 18:25:53 -0500

    Card style footer links

--- react-app/src/components/Footer/Technology.js
+++ react-app/src/components/Footer/Technology.js
@@ -2,17 +2,18 @@ import React from 'react'
 
 export function Technology({img, url}) {
   return (
+    <a href={url}>
     <div className='footer__tech-container'>
       <div className='footer__tech-image' style={{backgroundImage:`url(${img})`}}/>
-    </div>
+    </div></a>
   )
 }
 
 export function GitHubLogo({name, url}) {
   return (
-  <div className='footer__gh-container'>
+    <a href={url}><div className='footer__gh-container'>
       <div className='footer__gh-image' style={{backgroundImage:`url(https://logos-download.com/wp-content/uploads/2016/09/GitHub_logo.png)`}}/>
       <div className='footer__gh-name'>{name}</div>
-    </div>
+    </div></a>
   )
 }
