7ee1e5219df743b6ac2916d81d608caccce647e4
Clint Hill <clint.hill07@gmail.com>
2021-02-27 18:08:37 -0500

    Work on nav and footer clean up.

--- react-app/src/components/Footer/Technology.js
+++ react-app/src/components/Footer/Technology.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Technology({img}) {
+export function Technology({img, url}) {
   return (
     <div className='footer__tech-container'>
       <div className='footer__tech-image' style={{backgroundImage:`url(${img})`}}/>
@@ -8,4 +8,11 @@ function Technology({img}) {
   )
 }
 
-export default Technology
+export function GitHubLogo({name, url}) {
+  return (
+  <div className='footer__gh-container'>
+      <div className='footer__gh-image' style={{backgroundImage:`url(https://logos-download.com/wp-content/uploads/2016/09/GitHub_logo.png)`}}/>
+      <div className='footer__gh-name'>{name}</div>
+    </div>
+  )
+}
